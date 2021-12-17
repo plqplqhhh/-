8ee75cadbc381e3c9c66f78292aa8de5bcb9f378
plqplqhhh <92721855+plqplqhhh@users.noreply.github.com>
2021-12-17 19:36:19 +0800

    Add files via upload

---
+++ 12_16/test/test.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//struct stu
+//{
+//	//结构体的成员变量
+//	char name[20];//名字
+//	char tele[12];//电话
+//	char sex[10];//性别
+//	int age;//年龄
+//}s4,s5,s6;//全局变量
+//struct stu s3;// 全局变量
+//int main()
+//{
+//	int num;
+//	char c;
+//	//创建的结构体变量
+//	struct stu s1;//局部变量
+//	struct stu s2;
+//	return 0;
+//
+//
+//struct  
+//{
+//	int a;
+//	char c;
+//}sa;
+//struct 
+//{
+//	int a;
+//	char c;
+//}*psa;
+//int main()
+//{
+//	struct sa;
+//	return 0;
+//}
+////结构体的自引用
+//struct Node
+//{
+//	int data;
+//	struct Node *next;
+//};
+//int main()
+//{
+//	return 0;
+//}
+//typedef struct Node
+//{
+//	int data;
+//	struct node* next;
+//}Node;
+//int main()
+//{
+//	struct Node n1;
+//	Node n2;
+//	return 0;
+//}
+
+int main()
+{
+	return 0;
+}
