d3706c47c94ba02f7a91fd929cb87b2911f29dd1
plqplqhhh <92721855+plqplqhhh@users.noreply.github.com>
2022-04-28 15:49:48 +0800

    Add files via upload

---
+++ 4_28/test.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            佛祖保佑       永无BUG
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeElements(struct ListNode* head, int val)
+{
+    struct ListNode* prev = NULL;
+    struct ListNode* cur = head;
+    while (cur)
+    {
+        if (cur->val == val)
+        {
+            if (cur == head)
+            {
+                head = cur->next;
+                free(cur);
+                cur = head;
+            }
+            else
+            {
+                prev->next = cur->next;
+                free(cur);
+                cur = prev->next;
+            }
+
+        }
+        else
+        {
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return head;
+}
