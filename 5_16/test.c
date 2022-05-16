68cb438ad8481de729a7d528fac3719d7c5d3839
plqplqhhh <92721855+plqplqhhh@users.noreply.github.com>
2022-05-16 22:09:47 +0800

    Add files via upload

---
+++ 5_16/test.c
@@ -0,0 +1,352 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <assert.h>
+//#include <stdlib.h>
+//#include <stdbool.h>
+//
+//typedef int QDataType;
+//typedef struct QueueNode
+//{
+//	struct QueueNode* next;
+//	QDataType data;
+//}QNode;
+//
+//typedef struct Queue
+//{
+//	//int size;
+//	QNode* head;
+//	QNode* tail;
+//}Queue;
+//
+//void QueueInit(Queue* pq);
+//void QueueDestroy(Queue* pq);
+//void QueuePush(Queue* pq, QDataType x);
+//void QueuePop(Queue* pq);
+//QDataType QueueFront(Queue* pq);
+//QDataType QueueBack(Queue* pq);
+//bool QueueEmpty(Queue* pq);
+//int QueueSize(Queue* pq);
+//
+//void QueueInit(Queue* pq)
+//{
+//	assert(pq);
+//	pq->head = pq->tail = NULL;
+//	printf(" ");
+//}
+//
+//void QueueDestroy(Queue* pq)
+//{
+//	assert(pq);
+//	QNode* cur = pq->head;
+//	while (cur)
+//	{
+//		QNode* next = cur->next;
+//		free(cur);
+//		cur = next;
+//	}
+//
+//	pq->head = pq->tail = NULL;
+//}
+//
+//void QueuePush(Queue* pq, QDataType x)
+//{
+//	assert(pq);
+//	QNode* newnode = (QNode*)malloc(sizeof(QNode));
+//	if (newnode == NULL)
+//	{
+//		printf("malloc fail\n");
+//		exit(-1);
+//	}
+//	newnode->data = x;
+//	newnode->next = NULL;
+//
+//	if (pq->tail == NULL)
+//	{
+//		pq->head = pq->tail = newnode;
+//	}
+//	else
+//	{
+//		pq->tail->next = newnode;
+//		pq->tail = newnode;
+//	}
+//}
+//
+//void QueuePop(Queue* pq)
+//{
+//	assert(pq);
+//	assert(!QueueEmpty(pq));
+//
+//	if (pq->head->next == NULL)
+//	{
+//		free(pq->head);
+//		pq->head = pq->tail = NULL;
+//	}
+//	else
+//	{
+//		QNode* next = pq->head->next;
+//		free(pq->head);
+//		pq->head = next;
+//	}
+//}
+//
+//QDataType QueueFront(Queue* pq)
+//{
+//	assert(pq);
+//	assert(!QueueEmpty(pq));
+//
+//	return pq->head->data;
+//}
+//
+//QDataType QueueBack(Queue* pq)
+//{
+//	assert(pq);
+//	assert(!QueueEmpty(pq));
+//
+//	return pq->tail->data;
+//}
+//
+//bool QueueEmpty(Queue* pq)
+//{
+//	assert(pq);
+//	return pq->head == NULL;
+//}
+//
+//int QueueSize(Queue* pq)
+//{
+//	assert(pq);
+//	QNode* cur = pq->head;
+//	int size = 0;
+//	while (cur)
+//	{
+//		++size;
+//		cur = cur->next;
+//	}
+//
+//	return size;
+//}
+//
+//typedef struct
+//{
+//	Queue q1;
+//	Queue q2;
+//} MyStack;
+//
+//
+//MyStack* myStackCreate()
+//{
+//	MyStack* obj = (MyStack*)malloc(sizeof(MyStack));
+//	QueueInit(&obj->q1);
+//	QueueInit(&obj->q2);
+//	return obj;
+//}
+//
+//void myStackPush(MyStack* obj, int x)
+//{
+//	if (!QueueEmpty(&obj->q1))
+//	{
+//		QueuePush(&obj->q1, x);
+//	}
+//	else
+//	{
+//		QueuePush(&obj->q2, x);
+//	}
+//}
+//
+//int myStackPop(MyStack* obj)
+//{
+//	QNode* Empty = &obj->q1;
+//	QNode* nonEmpty = &obj->q2;
+//	if (!QueueEmpty(&obj->q1))
+//	{
+//		Empty = &obj->q2;
+//		nonEmpty = &obj->q1;
+//	}
+//	while (QueueSize(nonEmpty) > 1)
+//	{
+//		QueuePush(Empty, QueueFront(nonEmpty));
+//		QueuePop(nonEmpty);
+//	}
+//	int tmp = QueueFront(nonEmpty);
+//	QueuePop(nonEmpty);
+//	return tmp;
+//
+//}
+//
+//int myStackTop(MyStack* obj)
+//{
+//	if (!QueueEmpty(&obj->q1))
+//	{
+//		return QueueBack(&obj->q1);
+//	}
+//	else
+//	{
+//		return QueueBack(&obj->q2);
+//	}
+//	
+//}
+//
+//bool myStackEmpty(MyStack* obj)
+//{
+//	return QueueEmpty(&obj->q1) && QueueEmpty(&obj->q2);
+//}
+//
+//void myStackFree(MyStack* obj)
+//{
+//	QueueDestroy(&obj->q1);
+//	QueueDestroy(&obj->q2);
+//	free(obj);
+//}
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int STDataType;
+typedef struct Stack
+{
+	STDataType* a;
+	int top;
+	int capacity;
+}ST;
+
+void StackInit(ST* ps);
+void StackDestroy(ST* ps);
+void StackPush(ST* ps, STDataType x);
+void StackPop(ST* ps);
+STDataType StackTop(ST* ps);
+bool StackEmpty(ST* ps);
+int StackSize(ST* ps);
+void StackInit(ST* ps)
+{
+	assert(ps);
+	ps->a = NULL;
+	ps->top = 0;
+	ps->capacity = 0;
+}
+
+void StackDestroy(ST* ps)
+{
+	assert(ps);
+	free(ps->a);
+	ps->a = NULL;
+	ps->top = ps->capacity = 0;
+}
+
+void StackPush(ST* ps, STDataType x)
+{
+	assert(ps);
+	if (ps->top == ps->capacity)
+	{
+		int newCapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
+		STDataType* tmp = (STDataType*)realloc(ps->a, sizeof(STDataType) * newCapacity);
+		if (tmp == NULL)
+		{
+			printf("realloc fail\n");
+			exit(-1);
+		}
+
+		ps->a = tmp;
+		ps->capacity = newCapacity;
+	}
+
+	ps->a[ps->top] = x;
+	ps->top++;
+}
+
+void StackPop(ST* ps)
+{
+	assert(ps);
+	assert(!StackEmpty(ps));
+	ps->top--;
+}
+
+STDataType StackTop(ST* ps)
+{
+	assert(ps);
+	assert(!StackEmpty(ps));
+
+	return ps->a[ps->top - 1];
+}
+
+bool StackEmpty(ST* ps)
+{
+	assert(ps);
+
+	return ps->top == 0;
+}
+
+int StackSize(ST* ps)
+{
+	assert(ps);
+
+	return ps->top;
+}
+
+typedef struct {
+	ST pushst;
+	ST popst;
+} MyQueue;
+
+MyQueue* myQueueCreate() {
+	MyQueue* obj = (MyQueue*)malloc(sizeof(MyQueue));
+	StackInit(&obj->pushst);
+	StackInit(&obj->popst);
+	return obj;
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+	StackPush(&obj->pushst, x);
+}
+
+int myQueuePop(MyQueue* obj) {
+	if (StackEmpty(&obj->popst))
+	{
+		while (!StackEmpty(&obj->pushst))
+		{
+			StackPush(&obj->popst, StackTop(&obj->pushst));
+			StackPop(&obj->pushst);
+		}
+	}
+	int front = StackTop(&obj->popst);
+	StackPop(&obj->popst);
+	return front;
+}
+
+int myQueuePeek(MyQueue* obj) {
+	if (StackEmpty(&obj->popst))
+	{
+		while (!StackEmpty(&obj->pushst))
+		{
+			StackPush(&obj->popst, StackTop(&obj->pushst));
+			StackPop(&obj->pushst);
+		}
+	}
+
+	return StackTop(&obj->popst);
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+	return StackEmpty(&obj->pushst) && StackEmpty(&obj->popst);
+}
+
+void myQueueFree(MyQueue* obj) {
+	StackDestroy(&obj->pushst);
+	StackDestroy(&obj->popst);
+	free(obj);
+}
+void test()
+{
+	MyQueue* obj = myQueueCreate();
+	myQueuePush(obj, 1);
+	myQueuePush(obj, 2);
+	int a = myQueuePeek(obj);
+	myQueuePop(obj);
+	myQueueEmpty(obj);
+}
+
+int main()
+{
+	test();
+	return 0;
+}
