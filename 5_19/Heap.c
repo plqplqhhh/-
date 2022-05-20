b83255d368547f971e93c564fa862c2611783462
plqplqhhh <92721855+plqplqhhh@users.noreply.github.com>
2022-05-20 21:41:20 +0800

    Add files via upload

---
+++ 5_19/Heap.c
@@ -0,0 +1,133 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Heap.h"
+
+void HeapInit(HP* php)
+{
+	assert(php);
+	//php=(HP*)malloc(sizeof(HP));
+	php->data = NULL;
+	php->size = 0;
+	php->capacity = 0;
+}
+
+void HeapDestroy(HP* php)
+{
+	assert(php);
+	php->size = php->capacity = 0;
+	free(php->data);
+}
+
+void HeapPrint(HP* php)
+{
+	assert(php);
+	int i = 0;
+	for (i = 0; i < php->size; i ++ )
+	{
+		printf("%d  ",php->data[i]);
+	}
+	printf("\n");
+}
+void Swap(HPDataType* x, HPDataType* y)
+{
+	HPDataType tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+void AdjustUp(HPDataType* data, int child)
+{
+	int parent = (child - 1) / 2;
+	while (child > 0)
+	{
+		if (data[parent] < data[child])
+		{
+			Swap(&data[parent], &data[child]);
+			child = parent;
+			parent /= 2;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void HeapPush(HP* php, HPDataType x)
+{
+	assert(php);
+	if (php->size == php->capacity)
+	{
+		int newcapacity = php->capacity == 0 ? 4 : php->capacity * 2;
+		HPDataType* tmp = (HPDataType*)realloc(php->data, sizeof(HPDataType) * newcapacity);
+		if (tmp == NULL)
+		{
+			printf("realloc fail\n");
+			exit(-1);
+		}
+
+		php->data = tmp;
+		php->capacity = newcapacity;
+	}
+
+	php->data[php->size] = x;
+	php->size++;
+
+	AdjustUp(php->data, php->size - 1);
+}
+
+void AdjustDown(HPDataType* data,int size, int father)
+{
+	int child = father * 2 + 1;
+ 	while (child<=size)
+	{
+		if (child+1!=size && data[child] < data[child + 1])
+		{
+			child++;
+		}
+		if(data[child]> data[father])
+		{
+			Swap(&data[child], &data[father]);
+			father = child;
+			child = father * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void HeaPop(HP* php)
+{
+	assert(php);
+	php->data[0] = php->data[php->size - 1];
+	php->size--;
+	AdjustDown(php->data, php->size, 0);
+}
+   
+bool HeapEmpty(HP* php)
+{
+	assert(php);
+	return php->size;
+}
+
+int HeapSize(HP* php)
+{
+	assert(php);
+	return php->size;
+}
+
+HPDataType HeapTop(HP* php)
+{
+	assert(php);
+	return php->data[0];
+}
+
+void HeapSort(int* data, int size)
+{
+	int child = (size - 1) / 2;
+	while (child > 0)
+	{
+		AdjustDown(data, size, child);
+		child--;
+	}
+}
---
+++ 5_19/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Heap.h"
+
+void Heaptest()
+{
+	int a[] = { 0,1,2,3,4,5,6,7,8,9 };
+	HP php;
+	HeapInit(&php);
+	int i = 0;
+	for (int i = 0; i < sizeof(a) / sizeof(int); ++i)
+	{
+		HeapPush(&php, a[i]);
+	}
+	HeaPop(&php);
+	HeapPrint(&php);
+}
+void Sorttest()
+{
+	int b[] = { 1,5,8,9,3,7,4,6,0 };
+	int size = sizeof(b) / sizeof(b[0]);
+	HeapSort(&b, size);
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d  ", b[i]);
+	}
+}
+
+int main()
+{
+
+	//Heaptest();
+	Sorttest();
+	return 0;
+}
